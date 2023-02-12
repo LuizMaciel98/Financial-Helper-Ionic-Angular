790b8ad56ec5257b8ac6cc4f84a439eba4c091de
LuizMaciel98 <31411689+LuizMaciel98@users.noreply.github.com>
2023-02-12 20:41:13 -0300

    Splash art update on android

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Financial-Helper-Ionic-Angular
+# Finanças Inteligentes
 Esse projeto irá proporcionar um aplicativo Híbrido que lembrará você sobre as contas recorrentes a vencer mês a mês, ajudando você a planejar a sua vida financeira
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,9 +2,20 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.financasinteligentes',
-  appName: 'Financial-Helper-Ionic-Angular',
+  appName: 'Finanças Inteligentes',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      backgroundColor: "#000000",
+      launchShowDuration: 1000,
+      launchAutoHide: true,
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      splashFullScreen: true,
+      splashImmersive: true
+    },
+  },
 };
 
 export default config;

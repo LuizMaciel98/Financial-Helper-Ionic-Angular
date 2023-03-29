d2dd0c3138875769a994c3ac04706f6eaf5b5ee9
LuizMaciel98 <31411689+LuizMaciel98@users.noreply.github.com>
2023-03-29 19:00:51 -0300

    List Views Pages and List Components Creation

---
+++ src/app/components/bills-list/bills-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-bills-list',
+    templateUrl: './bills-list.component.html',
+    styleUrls: ['./bills-list.component.scss'],
+})
+export class BillsListComponent implements OnInit {
+
+  constructor() { }
+
+    ngOnInit() {
+        console.log();
+    }
+
+}
